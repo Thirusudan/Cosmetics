8a65e41e10bf0b741ff81102d563aa9bf11ee5ab
Thirusudan <thirusudan110@gmail.com>
2026-05-27 20:14:49 +0530

    product update function 🚀

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -60,3 +60,36 @@ export async function deleteProduct(req,res){
 
 }
 
+export async function updateProduct(req,res){
+    if(!isAdmin(req)){
+        res.status(403).json({message:"Access denined Admin only"})
+    }
+
+    const data = req.body; //1
+    const productId = req.params.productId; //2
+    data.productId=productId;  //3
+
+    try{
+        await Product.updateOne(
+            {
+                productId : productId,  //4
+            },
+            data //5
+        );
+        res.json({message:"Product updated sucessfully"})
+
+    }catch(error){
+        console.error("Error updating product:",error);
+        res.status(500).json({message:"Failed to update products"})
+        return
+    }
+}
+
+ 
+
+//1 - read the product details from varibale data (reads the body)
+//2 - we have to include product id in put request
+//3 - admin give a product id in put request and give other update details in the json ,url prodcut id(in the box) and product id in the json must be eqal (short version - the product id not allowed to update other details can be update)
+//4 - we have to give the product id when updating we creating 2nd function and using to the 4th fuction
+//5 - updated details we created in 1st function using to 5 
+
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { createProduct, deleteProduct, getProducts } from '../Controllers/productController.js';
+import { createProduct, deleteProduct, getProducts, updateProduct } from '../Controllers/productController.js';
 
 
 const productRouter = express.Router();
 productRouter.post("/",createProduct)
 productRouter.get("/",getProducts)
 productRouter.delete("/:productId",deleteProduct)
-
+productRouter.put("/:productId",updateProduct)
 
 export default productRouter;
 
